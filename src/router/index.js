0b16fac6b9563d6e411040a33672afe9b5e6644e
Administrator <admin@example.com>
2023-01-10 14:31:20 +0800

    feat: 直播间流量统计

--- src/router/index.js
+++ src/router/index.js
@@ -7,10 +7,24 @@ const router = new Router({
       path: '/live_play',
       component: () => import('../pages/room/index')
     },
+    {
+      name: 'traffic_statistic',
+      path: '/traffic_statistic',
+      component: () => import('../pages/traffic-statistics/index'),
+      meta: {
+        title: '流量统计'
+      }
+    },
   ],
   scrollBehavior() {
     return { x: 0, y: 0 }
   }
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) document.title = to.meta.title
+  if (to.query.token) localStorage.setItem('token', to.query.token)
+  next()
 })
 
 export default router
