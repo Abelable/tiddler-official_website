2cdecdea9d765277a6b1242065c10dd9c6b6e0ab
Administrator <admin@example.com>
2023-05-05 11:04:58 +0800

    feat: 调整禁言逻辑

--- src/pages/room/components/Comment.vue
+++ src/pages/room/components/Comment.vue
@@ -2,7 +2,7 @@
   <div class="msg-lists-wrap" ref="msgListsWrap" @touchstart="touchStart" @touchend="touchEnd" v-if="liveChatMsgList.length">
     <ul class="msg-lists">
       <li class="msg-list" v-for="(item, index) in liveChatMsgList" :key="index">
-        <template v-if="isAnchor || (!isAnchor && item.is_ban != 1) || (!isAnchor && item.is_ban == 1 && item.user_id == userID)">
+        <div class="msg-inner" v-if="isAnchor || (!isAnchor && item.is_ban != 1) || (!isAnchor && item.is_ban == 1 && item.user_id == userID)">
           <img class="avatar" v-if="item.head_img" :src="item.head_img" />
           <!-- <span 
             class="tag"
@@ -10,9 +10,9 @@
             v-if="isAnchor && item.type_name"
           >{{item.type_name}}</span> -->
           <span class="user-name" v-if="item.nick_name">{{(!isAnchor && anonymoused) ? (item.nick_name | sliceName) : item.nick_name}}</span>
-          <span class="message-content" v-if="item.is_ban == 1">（已禁言）</span>
+          <span class="message-content" v-if="item.is_ban == 1 && isAnchor">（已禁言）</span>
           <span class="message-content">{{item.message}}</span>
-        </template>
+        </div>
       </li>
     </ul>
   </div>
@@ -97,47 +97,48 @@ export default {
     display none
   .msg-lists
     .msg-list
-      margin-bottom .12rem
-      padding .18rem
-      width fit-content
-      max-width 5.46rem
-      font-size .26rem
-      font-weight 500
-      color #fff
-      line-height 1.5
-      border-radius .2rem
-      background-color rgba(0, 0, 0, 0.3)
-      .avatar
-        margin-right: .1rem
-        margin-top: -0.06rem
-        vertical-align: top
-        width: .52rem
-        height: .52rem
-        border-radius: 50%
-      .tag
-        display: inline-block
-        margin-right: .1rem
-        vertical-align: top
-        padding: 0 .12rem
-        height: .36rem
-        font-size: .22rem
-        font-weight: bold
-        line-height: .36rem
-        border-radius: .06rem
-        &.anchor
-          color: #FFE5BD
-          background: linear-gradient(128deg, #404A5C 0%, #0F131A 100%)
-        &.assistant
-          color: #404A5C
-          background: #E7CCFF
-        &.live-assistant
-          color: #404A5C
-          background: #FFBCDF
-      .user-name
-        vertical-align top
-        margin-right .1rem
-        color #EAAB63
-      .message-content
-        vertical-align top
+      .msg-inner
+        margin-bottom .12rem
+        padding .18rem
+        width fit-content
+        max-width 5.46rem
+        font-size .26rem
+        font-weight 500
         color #fff
+        line-height 1.5
+        border-radius .2rem
+        background-color rgba(0, 0, 0, 0.3)
+        .avatar
+          margin-right: .1rem
+          margin-top: -0.06rem
+          vertical-align: top
+          width: .52rem
+          height: .52rem
+          border-radius: 50%
+        .tag
+          display: inline-block
+          margin-right: .1rem
+          vertical-align: top
+          padding: 0 .12rem
+          height: .36rem
+          font-size: .22rem
+          font-weight: bold
+          line-height: .36rem
+          border-radius: .06rem
+          &.anchor
+            color: #FFE5BD
+            background: linear-gradient(128deg, #404A5C 0%, #0F131A 100%)
+          &.assistant
+            color: #404A5C
+            background: #E7CCFF
+          &.live-assistant
+            color: #404A5C
+            background: #FFBCDF
+        .user-name
+          vertical-align top
+          margin-right .1rem
+          color #EAAB63
+        .message-content
+          vertical-align top
+          color #fff
 </style>
