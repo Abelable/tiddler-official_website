22d9033e9626a8e9e91c13542ad8089db6ecfb7e
Administrator <admin@example.com>
2023-05-11 18:59:30 +0800

    feat: 配置webview

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -9,9 +9,9 @@ export default {
   created() {
     const token = this.$route.query.token || getUrlParam('token') || ''
     if (!token) {
-      const login_type = window.location.href.includes('sm') ? 2 : 1
+      const login_type = 1
       const state = encodeURIComponent(`login_type=${login_type}`)
-      window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx834688294afd4d48&redirect_uri=https%3A%2F%2Fyoubojia.youboi.com%2Fofficial-account%2Foauth-callback&response_type=code&scope=snsapi_userinfo&state=${state}#wechat_redirect`
+      window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx21737bccb934bd94&redirect_uri=https%3A%2F%2Fapi.talking.vip%2Fofficial-account%2Foauth-callback&response_type=code&scope=snsapi_userinfo&state=${state}#wechat_redirect`
       return
     } else {
       window.wx.miniProgram.navigateBack()
