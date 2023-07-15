cd64452f4ebb291862608a88f199d5c21840a2e9
Administrator <admin@example.com>
2023-07-15 20:43:12 +0800

    feat: ai

--- src/service/base/index.js
+++ src/service/base/index.js
@@ -1,89 +1,102 @@
-import axios from 'axios'
-import qs from 'qs'
-import { Toast } from 'vant'
-import { env } from '../../utils/config'
-import { judgeHost } from '../../utils/bridge'
-import sign from './sign'
+import axios from "axios";
+import qs from "qs";
+import { Toast } from "vant";
+import { env } from "../../utils/config";
+import { judgeHost } from "../../utils/bridge";
+import sign from "./sign";
 
 class Base {
   constructor() {
     switch (env) {
-      case 'pro':
-        this.mmsUrl = 'https://mms.youboi.com'
-        this.liveUrl = 'https://api.talking.vip'
-        break
+      case "pro":
+        this.mmsUrl = "https://mms.youboi.com";
+        this.liveUrl = "https://api.talking.vip";
+        break;
 
-      case 'dev':
-        this.mmsUrl = 'https://mms.youboe.com'
-        this.liveUrl = 'https://api.talking.vip'
-        break
+      case "dev":
+        this.mmsUrl = "https://mms.youboe.com";
+        this.liveUrl = "https://api.talking.vip";
+        break;
     }
   }
 
   async get(url, params, success, fail) {
-    return await this._axios({ url, params, success, fail })
+    return await this._axios({ url, params, success, fail });
   }
 
   async post(url, data, success, fail) {
-    return await this._axios({ method: 'POST', url, data, success, fail })
+    return await this._axios({ method: "POST", url, data, success, fail });
   }
 
-  async _axios({ method = 'GET', url, params, data, success, fail }) {
-    axios.defaults.headers['platform'] = 'official_account'
+  async _axios({ method = "GET", url, params, data, success, fail }) {
+    axios.defaults.headers["platform"] = "official_account";
     switch (judgeHost()) {
-      case 'IOS':
-        axios.defaults.headers['application_key'] = 'ybj_ios' 
+      case "IOS":
+        axios.defaults.headers["application_key"] = "ybj_ios";
         break;
 
-      case 'android':
-        axios.defaults.headers['application_key'] = 'ybj_android' 
+      case "android":
+        axios.defaults.headers["application_key"] = "ybj_android";
         break;
-    
-      case 'mp':
-        axios.defaults.headers['application_key'] = 'ybj_xcx' 
+
+      case "mp":
+        axios.defaults.headers["application_key"] = "ybj_xcx";
         break;
-    
+
       default:
-        axios.defaults.headers['application_key'] = 'ybj_h5' 
+        axios.defaults.headers["application_key"] = "ybj_h5";
         break;
     }
-    axios.defaults.headers['app_name'] = 'ruibo' 
-    if (method === 'POST') axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded'
-    const token = localStorage.getItem('token')
-    if (token) axios.defaults.headers['token'] = token
-    const userId = localStorage.getItem('userId')
-    if (token) axios.defaults.headers['uid'] = btoa(encodeURI(`${userId}`))
+    axios.defaults.headers["app_name"] = "ruibo";
+    if (method === "POST") {
+      axios.defaults.headers["Content-Type"] =
+        "application/x-www-form-urlencoded";
+    }
+    
+    const token = localStorage.getItem("token");
+    if (token) axios.defaults.headers["token"] = token;
+    
+    const userId = localStorage.getItem("userId");
+    if (userId && url.includes(this.mmsUrl)) {
+      axios.defaults.headers["uid"] = btoa(encodeURI(`${userId}`));
+    }
 
-    let res = await axios({ method, url, params, 
-      data: (method === 'POST' && url.indexOf(this.liveUrl) >= 0) ? qs.stringify(sign(data)) : qs.stringify(data),
+    let res = await axios({
+      method,
+      url,
+      params,
+      data:
+        method === "POST" && url.indexOf(this.liveUrl) >= 0
+          ? qs.stringify(sign(data))
+          : qs.stringify(data),
     }).catch(() => {
-      localStorage.removeItem('token')
-      Toast('服务繁忙, 请重试')
-    })
+      localStorage.removeItem("token");
+      Toast("服务繁忙, 请重试");
+    });
 
     if (res) {
       if ([200, 201, 204].includes(res.status)) {
         if ([200, 1001].includes(res.data.code)) {
-          if (success) success(res.data.data)
-          else return res.data.data
+          if (success) success(res.data.data);
+          else return res.data.data;
         } else if ([4040, 0].includes(res.data.code)) {
           if (fail) {
-            fail(res)
+            fail(res);
           } else {
-            localStorage.removeItem('token')
-            Toast('身份已失效，请重新点击链接进入')
-            return false
+            localStorage.removeItem("token");
+            Toast("身份已失效，请重新点击链接进入");
+            return false;
           }
         } else {
-          fail ? fail(res) : Toast(res.data.message)
-          return false
+          fail ? fail(res) : Toast(res.data.message);
+          return false;
         }
       } else {
-        Toast(res.errMsg)
-        return false
+        Toast(res.errMsg);
+        return false;
       }
     }
   }
 }
 
-export default Base
+export default Base;
