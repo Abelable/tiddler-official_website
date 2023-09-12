a0d9397b13930cf6af84bb907d1732c294e08060
Administrator <admin@example.com>
2023-09-12 10:45:50 +0800

    feat: 数字人我的页面

--- src/pages/digital-human/mine.vue
+++ src/pages/digital-human/mine.vue
@@ -1,25 +1,95 @@
 <template>
   <div>
-    <img
-      src="./images/home/bg_0.png"
-      alt=""
-      class="bg"
-    />
+    <img src="./images/home/bg_0.png" alt="" class="bg" />
 
     <div class="header">
       <div class="user-info">
-        <img src="./images/temp/avatar-1.jpg" alt="" class="avatar">
+        <img src="./images/temp/avatar-1.jpg" alt="" class="avatar" />
         <div class="nickname">昵称</div>
       </div>
 
       <div class="equity-wrap">
         <div class="date-wrap">
           <div class="valid-date">有效期至2023-05-23</div>
-          <img src="./images/mine/recharge.png" alt="" class="recharge-btn">
+          <img src="./images/mine/recharge.png" alt="" class="recharge-btn" />
         </div>
         <div class="equity-list">
-          <div class="equity">形象定制权益 <span class="equity-num">1/2</span> 个</div>
-          <div class="equity">声音定制权益 <span class="equity-num">0/2</span> 个</div>
+          <div class="equity">
+            形象定制权益 <span class="equity-num">1/2</span> 个
+          </div>
+          <div class="equity">
+            声音定制权益 <span class="equity-num">0/2</span> 个
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="content">
+      <div class="menu-list">
+        <div
+          class="menu-item"
+          :class="{ active: curMenuIdx === index }"
+          v-for="(item, index) in ['形象', '声音']"
+          :key="index"
+          @click="curMenuIdx = index"
+        >
+          {{ item }}
+        </div>
+      </div>
+
+      <div class="category-list">
+        <div
+          class="category-item"
+          :class="{ active: curCategoryIdx === index }"
+          v-for="(item, index) in ['全部', '已入驻', '未入驻', '制作中']"
+          :key="index"
+          @click="curCategoryIdx = index"
+        >
+          {{ item }}
+        </div>
+      </div>
+
+      <div class="video-list" v-if="curMenuIdx === 0">
+        <div class="video-item">
+          <img src="./images/temp/video-1.jpg" alt="" class="video-cover">
+          <div class="video-status success">已入驻</div>
+          <div class="action-num">动作库：3</div>
+          <div class="video-date">06-20 06:33</div>
+        </div>
+        <div class="video-item">
+          <img src="./images/temp/video-2.jpg" alt="" class="video-cover">
+          <div class="video-status fail">
+            <div>未入驻</div>
+            <img src="./images/mine/question.png" alt="" class="tips-icon">
+          </div>
+          <div class="video-date">06-20 06:33</div>
+        </div>
+        <div class="video-item">
+          <img src="./images/temp/video-1.jpg" alt="" class="video-cover">
+          <div class="video-status doing">制作中</div>
+          <div class="video-date">06-20 06:33</div>
+        </div>
+      </div>
+
+      <div class="voice-list" v-if="curMenuIdx === 1">
+        <div class="voice-item">
+          <div class="voice-status success">已入驻</div>
+          <img src="./images/temp/avatar-1.jpg" alt="" class="voice-avatar">
+          <div class="voice-name">白执法</div>
+          <div class="voice-identity">元气少女</div>
+          <img src="./images/voice-wave.png" alt="" class="voice-wave">
+          <div class="voice-date">06-20 06:33</div>
+        </div>
+        <div class="voice-item">
+          <div class="voice-status fail">
+            <div>未入驻</div>
+            <img src="./images/mine/question.png" alt="" class="tips-icon">
+          </div>
+          <img src="./images/temp/avatar-2.jpg" alt="" class="voice-avatar">
+          <div class="voice-name">楼可怜</div>
+          <div class="voice-identity">元气少女</div>
+          <img src="./images/voice-wave.png" alt="" class="voice-wave">
+          <div class="voice-date">06-20 06:33</div>
         </div>
       </div>
     </div>
@@ -28,8 +98,13 @@
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      curMenuIdx: 0,
+      curCategoryIdx: 0,
+    };
+  },
+};
 </script>
 
 <style lang="stylus" scoped>
@@ -86,4 +161,166 @@ export default {
         .equity-num
           color: #333
           font-weight: 500
+.content
+  padding-bottom: 1.3rem
+  min-height: calc(100vh - 4.8rem)
+  background: #fff
+  border-radius: .4rem .4rem 0 0
+  .menu-list
+    display: flex
+    .menu-item
+      display: flex
+      align-items: center
+      justify-content: center
+      flex: 1
+      height: 1.1rem
+      color: #999
+      font-size: .36rem
+      &.active
+        position: relative
+        color: #333
+        font-weight: 500
+        &::after
+          position: absolute
+          left: 50%
+          bottom: .18rem
+          transform: translateX(-50%)
+          content: ''
+          width: .72rem
+          height: .08rem
+          border-radius: .04rem
+          background: linear-gradient(135deg, #FDABE4 0%, #A4DCFD 100%)
+  .category-list
+    display: flex
+    .category-item
+      display: flex
+      align-items: center
+      justify-content: center
+      flex: 1
+      height: 1rem
+      color: #999
+      font-size: .28rem
+      &.active
+        color: #333
+        font-weight: 500
+  .video-list
+    display: flex
+    justify-content: space-between
+    flex-wrap: wrap
+    padding: 0 .4rem
+    .video-item
+      position: relative
+      margin-bottom: .18rem
+      font-size: 0
+      width: 3.24rem
+      height: 4.4rem
+      border-radius: .16rem
+      overflow: hidden
+      .video-cover
+        width: 100%
+        height: 100%
+        object-fit: cover
+      .video-status
+        position: absolute
+        top: 0
+        left: 0
+        display: flex
+        align-items: center
+        padding: 0 .1rem
+        height: .4rem
+        font-size: .24rem
+        border-bottom-right-radius: .16rem
+        &.success
+          color: #070F8F
+          background: linear-gradient(135deg, #FDABE4 0%, #A4DCFD 100%)
+        &.fail
+          color: #575B66
+          background: #CCCCCC
+        &.doing
+          color: #fff
+          background: #3841CB
+        .tips-icon
+          width: .28rem
+          height: .28rem
+      .action-num
+        position: absolute
+        top: 0
+        right : 0
+        display: flex
+        align-items: center
+        padding: 0 .1rem
+        height: .4rem
+        color: #fff
+        font-size: .24rem
+        border-bottom-left-radius: .16rem
+        background: linear-gradient(90deg, #7DDFDF 0%, #99ABFF 100%)
+      .video-date
+        position: absolute
+        left: 0
+        bottom: 0
+        display: flex
+        padding: .4rem .12rem .12rem
+        width: 100%
+        color: #fff
+        font-size: .24rem
+        background: linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.3) 100%)
+  .voice-list
+    display: flex
+    justify-content: space-between
+    flex-wrap: wrap
+    padding: 0 .4rem
+    .voice-item
+      position: relative
+      display: flex
+      flex-direction: column
+      align-items: center
+      justify-content: center
+      margin-bottom: .18rem
+      font-size: 0
+      width: 3.24rem
+      height: 3.24rem
+      border-radius: .16rem
+      overflow: hidden
+      background: #F5F7FC
+      .voice-status
+        position: absolute
+        top: 0
+        left: 0
+        display: flex
+        align-items: center
+        padding: 0 .1rem
+        height: .4rem
+        font-size: .24rem
+        border-bottom-right-radius: .16rem
+        &.success
+          color: #070F8F
+          background: linear-gradient(135deg, #FDABE4 0%, #A4DCFD 100%)
+        &.fail
+          color: #575B66
+          background: #CCCCCC
+        .tips-icon
+          width: .28rem
+          height: .28rem
+      .voice-avatar
+        width: .84rem
+        height: .84rem
+        border-radius: 50%
+      .voice-name
+        margin-top: .1rem
+        color: #575B66
+        font-size: .24rem
+      .voice-identity
+        margin-top: .06rem
+        color: #999
+        font-size: .24rem
+      .voice-wave
+        margin-top: .1rem
+        width: 1.4rem
+        height: .34rem
+      .voice-date
+        position: absolute
+        left: .16rem
+        bottom: .16rem
+        color: #999
+        font-size: .24rem
 </style>
