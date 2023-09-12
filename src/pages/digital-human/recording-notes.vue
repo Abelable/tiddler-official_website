b0fceee98bbdfc99a9ce2894b8d2b6a030c3786b
Administrator <admin@example.com>
2023-09-12 13:41:47 +0800

    feat: 数字人

---
+++ src/pages/digital-human/recording-notes.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>注意事项</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
--- src/router/routes/digital-human.js
+++ src/router/routes/digital-human.js
@@ -7,6 +7,14 @@ const digitalHuman = [
       title: "有播数字人",
     },
   },
+  {
+    name: "digital_human_recording_notes",
+    path: "/digital_human/recording_notes",
+    component: () => import("@/pages/digital-human/recording-notes"),
+    meta: {
+      title: "有播数字人",
+    },
+  },
 ];
 
 export default digitalHuman;
