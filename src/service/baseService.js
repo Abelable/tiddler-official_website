f9c59c78d541305c3991a68e0c4a34116748ff91
Administrator <admin@example.com>
2023-09-19 17:52:50 +0800

    feat: 商家入驻

--- src/service/baseService.js
+++ src/service/baseService.js
@@ -1,6 +1,9 @@
 import Base from './base/index'
 
 class BaseService extends Base {
+  async getOssInfo() {
+    return await this.post(`${this.liveUrl}/common/ali-sts`);
+  }
 }
 
 export default BaseService
