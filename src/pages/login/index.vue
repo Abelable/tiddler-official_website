c7f1de68184cd6a4db33b74ddf8f8639a2159126
Administrator <admin@example.com>
2023-10-16 17:21:58 +0800

    feat: line登录

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -19,11 +19,8 @@
             </ul>
             <template #reference>
               <div class="area-code-pikcer" v-if="areaCodeList.length">
-                <div>+{{areaCodeList[curAreaCodeIdx].value}}</div>
-                <img
-                  class="down-arrow"
-                  src="./images/down-arrow.png"
-                />
+                <div>+{{ areaCodeList[curAreaCodeIdx].value }}</div>
+                <img class="down-arrow" src="./images/down-arrow.png" />
               </div>
             </template>
           </Popover>
@@ -37,14 +34,38 @@
           </div>
         </div>
       </div>
-      <button class="login-btn" :class="{ active: code }" @click="login">登录</button>
+      <button class="login-btn" :class="{ active: code }" @click="login">
+        登录
+      </button>
     </div>
     <div class="protocol">
-        注册即代表您同意<a
-          href="https://h5.talking.vip/youbo_plus/h5/index.html#/privacy_policy"
-          >《瑞播用户服务协议》</a
-        >
+      注册即代表您同意<a
+        href="https://h5.talking.vip/youbo_plus/h5/index.html#/privacy_policy"
+        >《瑞播用户服务协议》</a
+      >
+    </div>
+
+    <div class="other-login">
+      <img src="./images/title.png" alt="" class="other-login-title">
+      <div class="login-btn-wrap">
+        <div class="login-btn" @click="lineLogin">
+          <img src="./images/line.png" alt="" class="login-btn-icon">
+          <div class="login-btn-desc">LINE</div>
+        </div>
+        <div class="login-btn">
+          <img src="./images/tiktok.png" alt="" class="login-btn-icon">
+          <div class="login-btn-desc">TikTok</div>
+        </div>
+        <div class="login-btn">
+          <img src="./images/wechat.png" alt="" class="login-btn-icon">
+          <div class="login-btn-desc">微信</div>
+        </div>
+        <div class="login-btn">
+          <img src="./images/facebook.png" alt="" class="login-btn-icon">
+          <div class="login-btn-desc">facebook</div>
+        </div>
       </div>
+    </div>
   </div>
 </template>
 
@@ -73,12 +94,8 @@ export default {
 
   async created() {
     const ua = navigator.userAgent.toLowerCase();
-    
-    if (ua.match(/wxwork/i)) {
-      // 企业微信环境
-      this.loginPageVisible = true;
-      this.setAreaCodeList();
-    } else {
+
+    if (ua.match(/MicroMessenger/i) || ua.match(/line/i)) {
       const isAuthCallback = localStorage.getItem("isAuthCallback");
       if (!isAuthCallback) {
         const { redirect = "" } = this.$route.query || {};
@@ -91,63 +108,48 @@ export default {
           window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx21737bccb934bd94&redirect_uri=https%3A%2F%2Fapi.talking.vip%2Fofficial-account%2Foauth-callback&response_type=code&scope=snsapi_userinfo&state=${state}#wechat_redirect`;
         } else if (ua.match(/line/i)) {
           // line环境
-          window.location.href = await this.setLineLoginUrl()
+          window.location.href = await this.setLineLoginUrl();
         }
       } else {
-        localStorage.removeItem("isAuthCallback");
-
-        const redirect = localStorage.getItem("redirect");
-        if (redirect) {
-          const token = this.$route.query.token || getUrlParam("token") || "";
-          localStorage.setItem("token", token);
-          localStorage.removeItem("redirect");
-          this.$router.push(`${redirect}`);
-        } else {
-          window.wx.miniProgram.navigateBack();
-        }
+        this.finishLogin()
+      }
+    } else {
+      const isAuthCallback = localStorage.getItem("isAuthCallback");
+      if (isAuthCallback) {
+        this.finishLogin()
+      } else {
+        this.loginPageVisible = true;
+        this.setAreaCodeList();
       }
     }
-
-    // if (
-    //   (ua.match(/wxwork/i) &&
-    //     ua.match(/wxwork/i).length &&
-    //     ua.match(/wxwork/i)[0] === "wxwork") ||
-    //   !ua.match(/MicroMessenger/i)
-    // ) {
-    //   this.loginPageVisible = true;
-    //   this.setAreaCodeList();
-    // } else {
-    //   // 公众号授权返回标识
-    //   const isWxAuthCallback = localStorage.getItem("isWxAuthCallback");
-
-    //   if (!isWxAuthCallback) {
-    //     // 公众号授权
-    //     const { redirect = "" } = this.$route.query || {};
-    //     redirect && localStorage.setItem("redirect", redirect);
-    //     localStorage.setItem("isWxAuthCallback", true);
-
-    //     const state = encodeURIComponent("login_type=1");
-    //     window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx21737bccb934bd94&redirect_uri=https%3A%2F%2Fapi.talking.vip%2Fofficial-account%2Foauth-callback&response_type=code&scope=snsapi_userinfo&state=${state}#wechat_redirect`;
-    //   } else {
-    //     localStorage.removeItem("isWxAuthCallback");
-
-    //     const redirect = localStorage.getItem("redirect");
-    //     if (redirect) {
-    //       const token = this.$route.query.token || getUrlParam("token") || "";
-    //       localStorage.setItem("token", token);
-    //       localStorage.removeItem("redirect");
-    //       this.$router.push(`${redirect}`);
-    //     } else {
-    //       window.wx.miniProgram.navigateBack();
-    //     }
-    //   }
-    // }
   },
 
   methods: {
+    finishLogin() {
+      localStorage.removeItem("isAuthCallback");
+
+      const redirect = localStorage.getItem("redirect");
+      if (redirect) {
+        const token = this.$route.query.token || getUrlParam("token") || "";
+        localStorage.setItem("token", token);
+        localStorage.removeItem("redirect");
+        this.$router.push(`${redirect}`);
+      } else {
+        window.wx.miniProgram.navigateBack();
+      }
+    },
+
     async setLineLoginUrl() {
-      const { url } =  await loginService.getLineLoginUrl() || {}
-      return url
+      const { url } = (await loginService.getLineLoginUrl()) || {};
+      return url;
+    },
+
+    async lineLogin() {
+      const { redirect = "" } = this.$route.query || {};
+      redirect && localStorage.setItem("redirect", redirect);
+      localStorage.setItem("isAuthCallback", true);
+
+      window.location.href = await this.setLineLoginUrl();
     },
 
     async setAreaCodeList() {
@@ -165,13 +167,15 @@ export default {
     },
 
     selectAreaCode(index) {
-      this.curAreaCodeIdx = index
-      this.showPopover = false
+      this.curAreaCodeIdx = index;
+      this.showPopover = false;
     },
 
     sendCode() {
       if (!this.countDown && this.mobile) {
-        loginService.getSms(`${this.areaCodeList[this.curAreaCodeIdx].value}-${this.mobile}`);
+        loginService.getSms(
+          `${this.areaCodeList[this.curAreaCodeIdx].value}-${this.mobile}`
+        );
         this.setCountDown();
       }
     },
@@ -190,8 +194,12 @@ export default {
 
     async login() {
       if (this.code) {
-        const { token } = await loginService.login(`${this.areaCodeList[this.curAreaCodeIdx].value}-${this.mobile}`, this.code) || {};
-        const { redirect } = this.$route.query
+        const { token } =
+          (await loginService.login(
+            `${this.areaCodeList[this.curAreaCodeIdx].value}-${this.mobile}`,
+            this.code
+          )) || {};
+        const { redirect } = this.$route.query;
         if (redirect) {
           localStorage.setItem("token", token);
           this.$router.push(`${redirect}`);
@@ -217,7 +225,7 @@ export default {
     height 100%
     z-index -1
   .content
-    padding 1.2rem .4rem
+    padding 1.2rem .4rem .36rem
     .title
       color #333
       font-size .68rem
@@ -251,7 +259,7 @@ export default {
         .get-code-btn
           color: #DAB174
     .login-btn
-      margin .9rem auto
+      margin .9rem auto 0
       width 100%
       height 1rem
       color #fff
@@ -263,16 +271,40 @@ export default {
         color: #FFE5BD
         background: linear-gradient(128deg, #404A5C 0%, #0F131A 100%);
   .protocol
-    position: absolute
-    bottom: 1rem
-    left: 0
-    width: 100%
     color #A3A3A3
     font-size 10px
     text-align center
     a
       color #B2372B
       text-decoration none
+  .other-login
+    position: absolute
+    left: 50%;
+    bottom: 2rem
+    transform: translateX(-50%)
+    display: flex
+    flex-direction: column
+    align-items: center
+    width 100vw
+    .other-login-title
+      height: .4rem
+    .login-btn-wrap
+      display: flex
+      justify-content: space-around
+      margin-top: .4rem
+      padding: 0 .6rem
+      width 100%
+      .login-btn
+        display: flex
+        flex-direction: column
+        align-items: center
+        .login-btn-icon
+          width: .8rem
+          height: .8rem
+        .login-btn-desc
+          margin-top: .16rem
+          color: #333
+          font-size: .24rem
 .area-code-list
   height: 5rem
   overflow-y: scroll
