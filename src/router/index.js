168de15f1a895e601ddea6b56125a5165c398e17
Administrator <admin@example.com>
2023-10-23 10:38:39 +0800

    feat: whatsApp分享

--- src/router/index.js
+++ src/router/index.js
@@ -9,19 +9,36 @@ const router = new Router({
 });
 
 router.beforeEach((to, from, next) => {
-  if (to.name === 'live_play') {
+  if (to.name === "live_play") {
     if (
       !navigator.userAgent.match(
         /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|IEMobile)/i
       )
     ) {
-      window.location.href = `https://h5.talking.vip/pc/#/l_p?id=${to.query.id}&parent_user_id=${to.query.parent_user_id || ''}`;
+      window.location.href = `https://h5.talking.vip/pc/#/l_p?id=${
+        to.query.id
+      }&parent_user_id=${to.query.parent_user_id || ""}`;
       return;
     }
   }
 
   if (to.meta.title) document.title = to.meta.title;
-  
+
+  if (to.name === "live_play") {
+    const { cover, title } = to.query;
+    const head = document.getElementsByTagName("head");
+
+    const coverMeta = document.createElement("meta");
+    coverMeta.property = "og:image";
+    coverMeta.content = cover;
+    head[0].appendChild(coverMeta);
+
+    const titleMeta = document.createElement("meta");
+    titleMeta.property = "og:title";
+    titleMeta.content = title;
+    head[0].appendChild(titleMeta);
+  }
+
   const token = localStorage.getItem("token");
   if (to.meta.requireAuth) {
     if (!token) {
